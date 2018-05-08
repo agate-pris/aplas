0fdd234350581f8a481bff30d23d1d7a933749e0
agate-pris <agate-pris@users.noreply.github.com>
2018-05-08 21:47:42 +0900

    Add type alias vector_2

--- include/agate_pris/aplas/qvm.hpp
+++ include/agate_pris/aplas/qvm.hpp
@@ -1,1 +1,14 @@
+#ifndef AGATE_PRIS_APLAS_QVM_HPP
+#define AGATE_PRIS_APLAS_QVM_HPP
 
+#include <boost/qvm/vec.hpp>
+
+namespace agate_pris {
+namespace aplas {
+
+using vector_2 = boost::qvm::vec<float, 2>;
+
+}
+}
+
+#endif
