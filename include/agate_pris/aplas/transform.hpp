9ecd9dddaec8fae18f3ad4577d0bc751e0ea13d0
agate-pris <agate-pris@users.noreply.github.com>
2018-05-20 16:13:12 +0900

    Add m_local_scale to transform

--- include/agate_pris/aplas/transform.hpp
+++ include/agate_pris/aplas/transform.hpp
@@ -3,6 +3,7 @@
 
 #include <boost/qvm/quat.hpp>
 #include <boost/qvm/quat_operations.hpp>
+#include <boost/qvm/swizzle3.hpp>
 #include <boost/qvm/vec.hpp>
 #include <boost/qvm/vec_operations.hpp>
 
@@ -18,11 +19,13 @@ namespace aplas {
     private:
         vector_3_type m_local_position;
         quaternion_type m_local_rotation;
+        vector_3_type m_local_scale;
 
     public:
         inline transform()
             : m_local_position(boost::qvm::zero_vec<scalar_type, 3>())
             , m_local_rotation(boost::qvm::identity_quat<scalar_type>())
+            , m_local_scale(boost::qvm::scalar_cast<scalar_type>(boost::qvm::_111()))
         {
         }
         inline vector_3_type const& get_const_local_position() const
@@ -64,6 +67,26 @@ namespace aplas {
         inline void set_local_rotation(quaternion_type const& q)
         {
             get_mutable_local_rotation() = q;
+        }
+        inline vector_3_type const& get_const_local_scale() const
+        {
+            return m_local_scale;
+        }
+        inline vector_3_type const& get_local_scale() const
+        {
+            return get_const_local_scale();
+        }
+        inline vector_3_type& get_mutable_local_scale()
+        {
+            return const_cast<vector_3_type&>(get_const_local_scale());
+        }
+        inline vector_3_type& get_local_scale()
+        {
+            return get_mutable_local_scale();
+        }
+        inline void set_local_scale(vector_3 const& v)
+        {
+            get_mutable_local_scale() = v;
         }
     };
 }
