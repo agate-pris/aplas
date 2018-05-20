ee6fe40c74f9af06fcc6190042c2307c944dcf9b
agate-pris <agate-pris@users.noreply.github.com>
2018-05-20 16:06:29 +0900

    Add m_local_rotation to transform

--- include/agate_pris/aplas/transform.hpp
+++ include/agate_pris/aplas/transform.hpp
@@ -1,6 +1,8 @@
 #ifndef AGATE_PRIS_APLAS_TRANSFORM_HPP
 #define AGATE_PRIS_APLAS_TRANSFORM_HPP
 
+#include <boost/qvm/quat.hpp>
+#include <boost/qvm/quat_operations.hpp>
 #include <boost/qvm/vec.hpp>
 #include <boost/qvm/vec_operations.hpp>
 
@@ -11,13 +13,16 @@ namespace aplas {
     public:
         typedef Scalar scalar_type;
         typedef boost::qvm::vec<scalar_type, 3> vector_3_type;
+        typedef boost::qvm::quat<scalar_type> quaternion_type;
 
     private:
         vector_3_type m_local_position;
+        quaternion_type m_local_rotation;
 
     public:
         inline transform()
             : m_local_position(boost::qvm::zero_vec<scalar_type, 3>())
+            , m_local_rotation(boost::qvm::identity_quat<scalar_type>())
         {
         }
         inline vector_3_type const& get_const_local_position() const
@@ -39,6 +44,26 @@ namespace aplas {
         inline void set_local_position(vector_3_type const& v)
         {
             get_mutable_local_position() = v;
+        }
+        inline quaternion_type const& get_const_local_rotation() const
+        {
+            return m_local_rotation;
+        }
+        inline quaternion_type const& get_local_rotation() const
+        {
+            return get_const_local_rotation();
+        }
+        inline quaternion_type& get_mutable_local_rotation()
+        {
+            return const_cast<quaternion_type&>(get_const_local_rotation());
+        }
+        inline quaternion_type& get_local_rotation()
+        {
+            return get_mutable_local_rotation();
+        }
+        inline void set_local_rotation(quaternion_type const& q)
+        {
+            get_mutable_local_rotation() = q;
         }
     };
 }
